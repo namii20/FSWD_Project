5d250164084e606801deedbc8b2f877447f46d95
Riya Pal <palriya8815@gmail.com>
2026-07-27 16:44:36 +0530

    Backend

---
+++ Backend/server.js
@@ -0,0 +1,9 @@
+
+const app = require("./src/App");
+
+
+app.listen(8000,()=>{
+    
+    console.log("Server is live!")
+})
+
---
+++ Backend/src/App.js
@@ -0,0 +1,18 @@
+//Backend 
+// creating server
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+module.exports =app;
+
+
+
+
+
+
+
+
+
+
